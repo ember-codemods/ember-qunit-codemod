d009876b2bbd0f8f1bc900bd867e793fe43ff25f
Robert Jackson <rjackson@linkedin.com>
2017-10-18 10:22:33 -0400

    Initial test (failing).

---
+++ __testfixtures__/ember-qunit-codemod/basic-module-for.input.js
@@ -0,0 +1,8 @@
+import { moduleFor, test } from 'ember-qunit';
+
+moduleFor('service:foo-bar', 'Unit | Service | FooBar', {
+});
+
+// Replace this with your real tests.
+test('it exists', function(assert) {
+});
---
+++ __testfixtures__/ember-qunit-codemod/basic-module-for.output.js
@@ -0,0 +1,10 @@
+import { module, test } from 'qunit';
+import { setupTest } from 'ember-qunit';
+
+module('Unit | Service | FooBar', function(hooks) {
+  setupTest(hooks);
+
+  // Replace this with your real tests.
+  test('it exists', function(assert) {
+  });
+});
