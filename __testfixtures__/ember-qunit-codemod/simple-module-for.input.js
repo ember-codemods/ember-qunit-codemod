354d269d9d650a741cfc0607cb350ff4a94c3da9
Robert Jackson <rjackson@linkedin.com>
2017-10-19 15:52:40 -0400

    Add some multi-test examples.

--- __testfixtures__/ember-qunit-codemod/simple-module-for.input.js
+++ __testfixtures__/ember-qunit-codemod/simple-module-for.input.js
@@ -5,3 +5,14 @@ moduleFor('service:foo', 'Unit | Service | Foo');
 test('it happens', function() {
 
 });
+
+test('it happens again', function() {
+
+});
+
+// this one has comments
+test('it happens and again', function() {
+
+});
+
+skip('this is included');
--- __testfixtures__/ember-qunit-codemod/simple-module-for.output.js
+++ __testfixtures__/ember-qunit-codemod/simple-module-for.output.js
@@ -7,4 +7,15 @@ module('Unit | Service | Foo', function(hooks) {
   test('it happens', function() {
 
   });
+
+  test('it happens again', function() {
+
+  });
+
+  // this one has comments
+  test('it happens and again', function() {
+
+  });
+
+  skip('this is included');
 });
