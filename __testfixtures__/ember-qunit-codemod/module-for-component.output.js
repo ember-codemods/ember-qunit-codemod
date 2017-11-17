40c815cca7fb5785d9afb09f0c2f92a31e3454c3
Robert Jackson <rjackson@linkedin.com>
2017-11-17 12:57:49 -0500

    Use scoped import for @ember/test-helpers.

--- __testfixtures__/ember-qunit-codemod/module-for-component.output.js
+++ __testfixtures__/ember-qunit-codemod/module-for-component.output.js
@@ -1,6 +1,6 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest, setupTest } from 'ember-qunit';
-import { clearRender, render, settled } from 'ember-test-helpers';
+import { clearRender, render, settled } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 
 module('Integration | Component | FooBar', function(hooks) {
