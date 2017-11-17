8dfafd46d773f6859880fc96053c47301af1175b
Robert Jackson <rjackson@linkedin.com>
2017-11-17 13:42:07 -0500

    Fix `this.subject` for `moduleForModel` invocations without args.

--- __testfixtures__/ember-qunit-codemod/module-for-model.input.js
+++ __testfixtures__/ember-qunit-codemod/module-for-model.input.js
@@ -5,3 +5,11 @@ moduleForModel('foo', 'Unit | Model | foo');
 test('It transforms the subject', function(assert) {
   const model = this.subject();
 });
+
+moduleForModel('foo', 'Unit | Model | Foo', {
+  needs: ['serializer:foo']
+});
+
+test('uses store method', function (assert) {
+  let store = this.store();
+});
